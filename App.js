ea3f3eb42ee9c038771e713f956dcdc8ea43bf73
akr1am <tradakram14@gmail.com>
2026-02-07 18:26:21 +0100

    Revert "m"
    This reverts commit 34a20b8653065d0d544a6e90a4b21b5a48aed693.

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import MainNavigator from './components/MainNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-     <MainNavigator/>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>chatbot</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
--- constants/colors.js
+++
@@ -1,7 +0,0 @@
-export const Colors = {
-    greyBg: '#F5F6F2',
-    primary: '#5f71e4',
-    secondary : '#465EFC'
-    
-}
-export default Colors
